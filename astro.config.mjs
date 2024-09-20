7b1f52a73413a71e0af0382dfb80d7bba33c49db
David Fross <davidmfross@gmail.com>
2024-09-20 14:39:47 -0500

    removed partytown and google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 import sitemap from '@astrojs/sitemap'
-import partytown from '@astrojs/partytown'
 import icon from 'astro-icon'
 export default defineConfig({
 	site: 'https://mksw.netlify.app',
-	integrations: [
-		tailwind(),
-		vue(),
-		sitemap(),
-		partytown({
-			config: {
-				forward: ['dataLayer.push'],
-			},
-		}),
-		icon(),
-	],
+	integrations: [tailwind(), vue(), sitemap(), icon()],
 })
