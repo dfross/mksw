d242a737b4e9bfce4dfc9e94e7a8f58b175eb9ea
David Fross <davidmfross@gmail.com>
2024-09-27 09:34:23 -0500

    add playform inline

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
 import compressor from 'astro-compressor'
 import playformCompress from '@playform/compress'
+import playformInline from '@playform/inline'
 export default defineConfig({
 	site: 'https://mksw.netlify.app',
-	integrations: [tailwind(), vue(), sitemap(), icon(), playformCompress(), compressor()],
+	integrations: [tailwind(), vue(), sitemap(), icon(), playformCompress(), playformInline(), compressor()],
 })
