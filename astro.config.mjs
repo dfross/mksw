37fc7f32cd860cb5f83fe5473cfcd60be1a7eb8f
David Fross <davidmfross@gmail.com>
2024-09-27 09:30:11 -0500

    add playform-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
-import compressor from 'astro-compressor';
+import compressor from 'astro-compressor'
+import playformCompress from '@playform/compress'
 export default defineConfig({
-    site: 'https://mksw.netlify.app',
-    integrations: [tailwind(), vue(), sitemap(), icon(), compressor()],
+	site: 'https://mksw.netlify.app',
+	integrations: [tailwind(), vue(), sitemap(), icon(), playformCompress(), compressor()],
 })
