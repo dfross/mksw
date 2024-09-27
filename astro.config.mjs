c4481d680292d0e529a8c18adc16b84e42372926
David Fross <davidmfross@gmail.com>
2024-09-27 09:24:07 -0500

    add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind'
 import vue from '@astrojs/vue'
 import sitemap from '@astrojs/sitemap'
 import icon from 'astro-icon'
+import compressor from 'astro-compressor';
 export default defineConfig({
-	site: 'https://mksw.netlify.app',
-	integrations: [tailwind(), vue(), sitemap(), icon()],
+    site: 'https://mksw.netlify.app',
+    integrations: [tailwind(), vue(), sitemap(), icon(), compressor()],
 })
